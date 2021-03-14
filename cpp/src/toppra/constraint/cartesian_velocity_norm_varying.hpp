828c75b3f0310a1a4d18c8fef0a2528930c5bab6
Steve Golton <stevegolton@gmail.com>
2021-03-14 18:33:36 +0000

    Linearly interpolate limits between waypoints in all varying constraints

--- cpp/src/toppra/constraint/cartesian_velocity_norm_varying.hpp
+++ cpp/src/toppra/constraint/cartesian_velocity_norm_varying.hpp
@@ -28,17 +28,34 @@ public:
 
   void computeVelocityLimit(toppra::value_type time) override
   {
-    auto iter = std::lower_bound(m_times.begin(), m_times.end(), time);
-    if (iter == m_times.end())
+    auto second = std::lower_bound(m_times.begin(), m_times.end(), time);
+    if (second == m_times.end())
     {
-      // throw something here!
+      second--;
+      auto index = second - m_times.begin();
+      m_limit = m_limit_vec(index);
     }
-
-    auto in = iter - m_times.begin();
-    m_limit = m_limit_vec(in);
+    else if (second == m_times.begin())
+    {
+      m_limit = m_limit_vec(0);
+    }
+    else
+    {
+      auto first = second - 1;
+      auto a_index = first - m_times.begin();
+      auto a = *first;
+      auto b_index = second - m_times.begin();
+      auto b = *second;
+      auto alpha = (time - a) / (b - a);
+      m_limit = lerp(m_limit_vec(a_index), m_limit_vec(b_index), alpha);
+    }
   }
 
 private:
+  toppra::value_type lerp(toppra::value_type a, toppra::value_type b, double alpha)
+  {
+    return (a*(1.0-alpha) + b*alpha);
+  }
   toppra::Vector m_limit_vec;
   std::vector<toppra::value_type> m_times;
 }; // class LinearJointVelocityVarying
--- cpp/src/toppra/constraint/linear_joint_velocity_varying.hpp
+++ cpp/src/toppra/constraint/linear_joint_velocity_varying.hpp
@@ -29,19 +29,37 @@ public:
 
   void computeVelocityLimits(toppra::value_type time) override
   {
-    auto iter = std::lower_bound(m_times.begin(), m_times.end(), time);
-    if (iter == m_times.end())
+    auto second = std::lower_bound(m_times.begin(), m_times.end(), time);
+    if (second == m_times.end())
     {
-      // throw something here!
+      second--;
+      auto index = second - m_times.begin();
+      m_lower = m_lower_vec.row(index);
+      m_upper = m_upper_vec.row(index);
     }
-
-    auto in = iter - m_times.begin();
-
-    m_lower = m_lower_vec.row(in);
-    m_upper = m_upper_vec.row(in);
+    else if (second == m_times.begin())
+    {
+      m_lower = m_lower_vec.row(0);
+      m_upper = m_upper_vec.row(0);
+    }
+    else
+    {
+      auto first = second - 1;
+      auto a_index = first - m_times.begin();
+      auto a = *first;
+      auto b_index = second - m_times.begin();
+      auto b = *second;
+      auto alpha = (time - a) / (b - a);
+      m_lower = lerp(m_lower_vec.row(a_index), m_lower_vec.row(b_index), alpha);
+      m_upper = lerp(m_upper_vec.row(a_index), m_upper_vec.row(b_index), alpha);
+    }
   }
 
 private:
+  toppra::Vector lerp(const toppra::Vector &a, const toppra::Vector &b, double alpha)
+  {
+    return (a*(1.0-alpha) + b*alpha);
+  }
   toppra::Matrix m_lower_vec, m_upper_vec;
   std::vector<toppra::value_type> m_times;
 }; // class LinearJointVelocityVarying
